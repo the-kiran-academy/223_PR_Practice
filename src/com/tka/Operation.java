395da0d01650b0d1d084ef173360795d93e4a4b9
SnehalG08 <golamsnehal08@gmail.com>
2025-05-31 11:29:15 +0530

    fibbonacii series

--- src/com/tka/Operation.java
+++ src/com/tka/Operation.java
@@ -17,6 +17,22 @@ public class Operation {
 	}
 	
 	public static String fibonacci(int number) {
-		return null;
+		
+		if(number<=0)
+			return "";
+		if(number==1)
+			return "0";
+		 StringBuilder sb=new StringBuilder("0,1");
+		 int prev=0;
+		 int curr=1;
+		 
+		 for(int i=2;i<number;i++) {
+			 int next =prev+curr;
+			 sb.append(",").append(next);
+			 prev=curr;
+			 curr=next;
+		 }
+		
+		return sb.toString();
 	}
 }
--- src/com/tka/Test.java
+++ src/com/tka/Test.java
@@ -3,9 +3,9 @@ package com.tka;
 public class Test {
 
 	public static void main(String[] args) {
-		Operation.evenOdd(10);
-		Operation.prime(7);
-		Operation.palindrome(121);
+		//Operation.evenOdd(10);
+		//Operation.prime(7);
+		//Operation.palindrome(121);
 		Operation.fibonacci(8);
 
 	}
