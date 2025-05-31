982c8764b19c0d60c81b4cc0ffe76acf94cc2c65
ram-chadar <javaprojectwork2715@gmail.com>
2025-05-31 10:56:36 +0530

    first commit

---
+++ src/com/tka/Operation.java
@@ -0,0 +1,22 @@
+package com.tka;
+
+public class Operation {
+	
+	public static String evenOdd(int number) {
+       return null;
+    }
+	
+	
+	public static String prime(int number) {
+		return null;
+	}
+
+	
+	public static String palindrome(int number) {
+		return null;
+	}
+	
+	public static String fibonacci(int number) {
+		return null;
+	}
+}
---
+++ src/com/tka/Test.java
@@ -0,0 +1,13 @@
+package com.tka;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Operation.evenOdd(10);
+		Operation.prime(7);
+		Operation.palindrome(121);
+		Operation.fibonacci(8);
+
+	}
+
+}
