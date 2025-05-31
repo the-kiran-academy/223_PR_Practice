e135a30f27ec7b3bd5c1d00fb8c8b1d7d187b443
Sarangkar <shivanisarangkar@gmail.com>
2025-05-31 11:22:56 +0530

    Worked on Palindrome

--- src/com/tka/Test.java
+++ src/com/tka/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		Operation.evenOdd(10);
 		Operation.prime(7);
-		Operation.palindrome(121);
+		System.out.println(Operation.palindrome(122));
 		Operation.fibonacci(8);
 
 	}
