08f3d563a4a9d75ca566981ceeecdce1f76e701f
ROSHNI-LODE <loderoshni@gmail.com>
2025-05-31 11:24:24 +0530

    evenodd

--- src/com/tka/Operation.java
+++ src/com/tka/Operation.java
@@ -3,6 +3,12 @@ package com.tka;
 public class Operation {
 	
 	public static String evenOdd(int number) {
+		if (number % 2 == 0) {
+            System.out.println(number + " is Even.");
+        } else {
+            System.out.println(number + " is Odd.");
+        }
+
        return null;
     }
 	
--- src/com/tka/Test.java
+++ src/com/tka/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		Operation.evenOdd(10);
+		Operation.evenOdd(3);
+		
 		Operation.prime(7);
 		Operation.palindrome(121);
 		Operation.fibonacci(8);
